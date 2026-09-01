ddd77eb94cd2b53dc6a424eef54c465a5fed8257
milgo <develimgo@gmail.com>
2026-09-01 14:15:48 +0200

    initial commit

---
+++ clock.vhd
@@ -0,0 +1,108 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+ 
+entity tutorial_led_blink is
+  port (
+    i_CLK      : in  std_logic;
+    --i_EN      : in  std_logic;
+    i_SW : in  STD_LOGIC_VECTOR (1 downto 0);
+    o_LD  : out std_logic
+    );
+end tutorial_led_blink;
+ 
+architecture rtl of tutorial_led_blink is
+ 
+  -- Constants to create the frequencies needed:
+  -- Formula is: (8 MHz / 100 Hz * 50% duty cycle)
+  -- So for 100 Hz: 8,000,000 / 100 * 0.5 = 400,000
+  constant c_CNT_100HZ : natural := 40000;
+  constant c_CNT_50HZ  : natural := 80000;
+  constant c_CNT_10HZ  : natural := 400000;
+  constant c_CNT_1HZ   : natural := 4000000;
+ 
+ 
+  -- These signals will be the counters:
+  signal r_CNT_100HZ : natural range 0 to c_CNT_100HZ;
+  signal r_CNT_50HZ  : natural range 0 to c_CNT_50HZ;
+  signal r_CNT_10HZ  : natural range 0 to c_CNT_10HZ;
+  signal r_CNT_1HZ   : natural range 0 to c_CNT_1HZ;
+   
+  -- These signals will toggle at the frequencies needed:
+  signal r_TOGGLE_100HZ : std_logic := '0';
+  signal r_TOGGLE_50HZ  : std_logic := '0';
+  signal r_TOGGLE_10HZ  : std_logic := '0';
+  signal r_TOGGLE_1HZ   : std_logic := '0';
+ 
+  -- One bit select wire.
+  signal w_LED_SELECT : std_logic;
+   
+begin
+ 
+  -- All processes toggle a specific signal at a different frequency.
+  -- They all run continuously even if the switches are
+  -- not selecting their particular output.
+   
+  p_100_HZ : process (i_CLK) is
+  begin
+    if rising_edge(i_CLK) then
+      if r_CNT_100HZ = c_CNT_100HZ-1 then  -- -1, since counter starts at 0
+        r_TOGGLE_100HZ <= not r_TOGGLE_100HZ;
+        r_CNT_100HZ    <= 0;
+      else
+        r_CNT_100HZ <= r_CNT_100HZ + 1;
+      end if;
+    end if;
+  end process p_100_HZ;
+ 
+ 
+  p_50_HZ : process (i_CLK) is
+  begin
+    if rising_edge(i_CLK) then
+      if r_CNT_50HZ = c_CNT_50HZ-1 then  -- -1, since counter starts at 0
+        r_TOGGLE_50HZ <= not r_TOGGLE_50HZ;
+        r_CNT_50HZ    <= 0;
+      else
+        r_CNT_50HZ <= r_CNT_50HZ + 1;
+      end if;
+    end if;
+  end process p_50_HZ;
+ 
+   
+  p_10_HZ : process (i_CLK) is
+  begin
+    if rising_edge(i_CLK) then
+      if r_CNT_10HZ = c_CNT_10HZ-1 then  -- -1, since counter starts at 0
+        r_TOGGLE_10HZ <= not r_TOGGLE_10HZ;
+        r_CNT_10HZ    <= 0;
+      else
+        r_CNT_10HZ <= r_CNT_10HZ + 1;
+      end if;
+    end if;
+  end process p_10_HZ;
+ 
+   
+  p_1_HZ : process (i_CLK) is
+  begin
+    if rising_edge(i_CLK) then
+      if r_CNT_1HZ = c_CNT_1HZ-1 then  -- -1, since counter starts at 0
+        r_TOGGLE_1HZ <= not r_TOGGLE_1HZ;
+        r_CNT_1HZ    <= 0;
+      else
+        r_CNT_1HZ <= r_CNT_1HZ + 1;
+      end if;
+    end if;
+  end process p_1_HZ;
+ 
+   
+  -- Create a multiplexor based on switch inputs
+  w_LED_SELECT <= r_TOGGLE_100HZ when i_SW = "00" else
+                  r_TOGGLE_50HZ  when i_SW = "10" else
+                  r_TOGGLE_10HZ  when i_SW = "01" else
+                  r_TOGGLE_1HZ;
+ 
+   
+  -- Only allow o_led_drive to drive when i_enable is high (and gate).
+  o_LD <= w_LED_SELECT; --and i_enable;
+ 
+end rtl;
